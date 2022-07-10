da48301562fee2a0dda67644bfaba3b6bff12b26
a-almubarak <67047358+a-almubarak@users.noreply.github.com>
2022-07-10 06:13:47 +0300

    isogram

--- exercises/practice/isogram/isogram.c
+++ exercises/practice/isogram/isogram.c
@@ -1,1 +1,17 @@
 #include "isogram.h"
+
+bool is_character(char c){
+    return (c>='a' && z<='z') || (c>='A' && c<='Z');
+}
+
+bool is_isogram(const char phrase[]){
+    int size = 1024;
+    char seen[size] = {0};
+    for(int i=0;phrase[i];i++){
+        if(is_character(phrase[i]) && !seen[i])
+            seen[i] = i;
+        else
+            return false;
+    }
+    return true;
+}
